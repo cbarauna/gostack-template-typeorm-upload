b6f620a16906409f0160b61c8fdbac217989afd5
Carlos Baraúna <cbarauna@live.com>
2020-06-23 18:16:02 -0300

    Método Post

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,29 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm'
+
+import TransactionRepository from '../repositories/TransactionsRepository'
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const repositories = getCustomRepository(TransactionRepository)
+
+    const transactions = repositories.create({
+      title,
+      value,
+      type,
+    });
+
+    await repositories.save(transactions);
+
+    return transactions;
   }
 }
 
